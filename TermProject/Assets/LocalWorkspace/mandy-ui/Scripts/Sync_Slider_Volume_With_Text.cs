8008e78bb801c28031327d59fb9011a5fab78fc1
Mandy Noto <mandynoto@icloud.com>
2023-03-19 18:40:23 -0700

    feat(ui-settings): attach slider and volume text to slider

--- TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Sync_Slider_Volume_With_Text.cs
+++ TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Sync_Slider_Volume_With_Text.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:168c7c5b1479f6f00bfb919fd17cc7d6d5a57492df87a62840f9f3e198a235b9
+oid sha256:23933d606d76a80c4c1927d2418ab51d2f930c88cd1190a0ad2c706a2bf15388
 size 828
