4dc73347edd2e2c2879a24671f40cdcc38e90e3b
Mandy Noto <mandynoto@icloud.com>
2023-03-21 20:59:02 -0700

    docs(mandy-ui/Scripts): wrong description of scene transition

--- TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Exit_Application_Controller.cs
+++ TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Exit_Application_Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b66946f10748bce2711822c9a777d5f8de31075f18b64a14b7ccf7340a765544
-size 450
+oid sha256:82bd453d5b3077694295ab8dd92d261da3735408b41bfe4c79a891de267e6a37
+size 456
