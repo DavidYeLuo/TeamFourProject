311cb22b28352b39829cf3630b23ede653d10594
Mandy Noto <mandynoto@icloud.com>
2023-03-22 15:46:30 -0700

    refactor(Save_Search_Lobby_Input): rename input to searched_lobby_input
    this is to provide clarity for backend

--- TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Save_Search_Lobby_Input.cs
+++ TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Save_Search_Lobby_Input.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7ab00c3b144695f6ce2ae9603a8af5e749a724ad660f835dd871098ae3c303d
-size 438
+oid sha256:f0b3adb0caedd4de98ea16ea5511042bfe961a3d32183322c12331aa53bc9d4e
+size 468
