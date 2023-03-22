d40fd0ca6fad250ddce42035b7b6c51e2608afc7
Mandy Noto <mandynoto@icloud.com>
2023-03-22 15:37:38 -0700

    refactor(Save_Search_Lobby_Input): remove debug statement
    This debug statement triggered when input is saved 'Debug.Log(input);' inside 'ReadInputString' method, writing this because git-lfs

--- TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Save_Search_Lobby_Input.cs
+++ TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Save_Search_Lobby_Input.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52cbab6a22d403ecad4b4aaaefc7764b68d29c29a2807d25802f27cb16521a26
-size 465
+oid sha256:e7ab00c3b144695f6ce2ae9603a8af5e749a724ad660f835dd871098ae3c303d
+size 438
