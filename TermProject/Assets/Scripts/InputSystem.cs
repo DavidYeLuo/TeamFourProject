0731dfa5cb2049b50527d2dceef9d718b8e288a5
DavidYeLuo <davidayeluo@gmail.com>
2023-03-23 12:57:32 -0700

    Separate inputs from player controller.

---
+++ TermProject/Assets/Scripts/InputSystem.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a48cb0f43aeb95daa051a30d113eaed2287096d383e87b0b1a3311ae99d7b7e8
+size 1545
--- TermProject/Assets/Scripts/PlayerController.cs
+++ TermProject/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dbc2640d69f8f2deda5328312a6263d98d30e5cc9632d2d337b46fdbfaada08
-size 7807
+oid sha256:1d4d98afea493749000512ddd2b0105f9b86263f861b9b5e299acaa7b6f6b5e3
+size 7207
