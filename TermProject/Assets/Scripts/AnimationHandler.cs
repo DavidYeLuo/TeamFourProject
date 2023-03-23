f7ae39826cbd1560a09b68ddf086bcc3e4a64c73
DavidYeLuo <davidayeluo@gmail.com>
2023-03-23 15:00:28 -0700

    Separate jump animation from player controller. Add middleman(observer pattern) for booleans.

--- TermProject/Assets/Scripts/AnimationHandler.cs
+++ TermProject/Assets/Scripts/AnimationHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f914fcb6798f079f94880a70aabb80708abf03eee922e95d7d7929faa24e9e9
-size 1471
+oid sha256:93efff93db1a25c5928f9d0c6b0eff5498b13b768ce3b3c7a6f4022bb3afcae8
+size 1740
--- TermProject/Assets/Scripts/PlayerController.cs
+++ TermProject/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb1f45e81d846fccc3c0ecdaaf099432be8dc6e45dc9e900bdb9009f3a65493d
-size 7348
+oid sha256:45f3c9ed421c5ee92695eded632735d27172d29b3dd84c710ec53191fbed88d7
+size 5239
