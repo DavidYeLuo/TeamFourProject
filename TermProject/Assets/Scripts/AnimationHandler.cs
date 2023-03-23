a0f7ff0bd87c3d4f32448f4bf2e6459e55d52d9a
DavidYeLuo <davidayeluo@gmail.com>
2023-03-23 14:22:14 -0700

    Separate animation from player controller. Also add a middleman for velocity data. Middle man for the separation.

---
+++ TermProject/Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9f914fcb6798f079f94880a70aabb80708abf03eee922e95d7d7929faa24e9e9
+size 1471
--- TermProject/Assets/Scripts/PlayerController.cs
+++ TermProject/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d4d98afea493749000512ddd2b0105f9b86263f861b9b5e299acaa7b6f6b5e3
-size 7207
+oid sha256:fb1f45e81d846fccc3c0ecdaaf099432be8dc6e45dc9e900bdb9009f3a65493d
+size 7348
