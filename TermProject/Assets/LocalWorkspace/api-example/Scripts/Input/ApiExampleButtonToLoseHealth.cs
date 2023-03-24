95161c9a9bab0c6e252c867ff60127b440d54bd4
DavidYeLuo <dayeluo@galileoweb.org>
2023-03-24 01:45:07 -0700

    Delete API example because it doesn't fit well for multiplayer. Reworking player.

--- TermProject/Assets/LocalWorkspace/api-example/Scripts/Input/ApiExampleButtonToLoseHealth.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fb104d6bab959d0d6a7f4ff60c0fd6af7f463414b9d4e2a30540fb910d4be7bd
-size 508
---
+++ TermProject/Assets/Scripts/InputSystemRework.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5889aa9fbea21dd47a28719e4ca9351222fb7d334fb997c3b90b6e8bd05988a0
+size 1464
