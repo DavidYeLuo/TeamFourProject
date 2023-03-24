3c909e1d13bc73d13e73c879e4921bba19e42521
DavidYeLuo <dayeluo@galileoweb.org>
2023-03-24 03:30:32 -0700

    Add Jump but need a ground detector.

---
+++ TermProject/Assets/Scripts/Controllers/JumpController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:921534e769fb9cfe04fce6c21ad22bbeca6a7933f5e0ff1baf0c3ff4898e3bde
+size 326
--- TermProject/Assets/Scripts/PlayerHandler.cs
+++ TermProject/Assets/Scripts/PlayerHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ccc6c37eae6551db4b1747ddf6b099cced5c2a71bea484f8cbc4bf31f93d4d5
-size 2623
+oid sha256:9307dc8f6ae0d97f08fede7b862b1db74e08a2364bbd145d46757f4514d2ded7
+size 2788
