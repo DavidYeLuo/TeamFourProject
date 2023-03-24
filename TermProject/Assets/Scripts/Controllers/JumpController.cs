dcfbf90af8bf235f17b7024efeba354e7c09256d
DavidYeLuo <dayeluo@galileoweb.org>
2023-03-24 14:58:37 -0700

    Add Ground Detection for Jump

--- TermProject/Assets/Scripts/Controllers/JumpController.cs
+++ TermProject/Assets/Scripts/Controllers/JumpController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:921534e769fb9cfe04fce6c21ad22bbeca6a7933f5e0ff1baf0c3ff4898e3bde
-size 326
+oid sha256:6cd7bec30300618f312f3dde32ff61c010d264fe5bd0c431caa57d3b1666b8c6
+size 772
