1ceedb2472335e5abff476c28b45ee87d4ba936d
DavidYeLuo <davidayeluo@gmail.com>
2023-03-23 19:32:24 -0700

    Add camera to player. Fix animation not matching where the player is going.

--- TermProject/Assets/Scripts/PlayerController.cs
+++ TermProject/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd4bfb8cea946481d39f3e89f02c1d43d3ae3beec99f46dace13cdefb35e5950
-size 5948
+oid sha256:f28166981d4a9c18ded8bb9f02cbe8bd6ca20ee719f86ebc42f5f9001d3899a7
+size 6312
