82eb80d4bb24086033adb828a1aee37be39be19e
DavidYeLuo <dayeluo@galileoweb.org>
2023-03-24 17:19:00 -0700

    Add Camera follow with Cinemamachine and movement is relative to Camera

--- TermProject/Assets/Scripts/Controllers/CustomRotation.cs
+++ TermProject/Assets/Scripts/Controllers/CustomRotation.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aa7b8dc68df7477aa52681f4f6a20502df895994e939c1ab678b7c3f22d6665
-size 232
+oid sha256:3f55b3ded36e011c5bc222d112cf6d9b4a1052171012889a1fbe896677f28d85
+size 452
--- TermProject/Assets/Scripts/PlayerHandler.cs
+++ TermProject/Assets/Scripts/PlayerHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7631540f8ed19b7894e01cfeab3dfe4db4bc84d507022294ef578207ac1897b
-size 2015
+oid sha256:b2c6ef53124099fb2f828b64d4e1e97b7f8c1c2e1c55ed48727a72feb69e92c8
+size 2682
