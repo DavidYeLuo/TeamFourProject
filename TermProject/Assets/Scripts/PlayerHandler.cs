4ea1d7a061670bf8b83957f9192bd2fd8a53f392
DavidYeLuo <dayeluo@galileoweb.org>
2023-03-24 20:48:57 -0700

    Add velocity data for animation

--- TermProject/Assets/Scripts/PlayerHandler.cs
+++ TermProject/Assets/Scripts/PlayerHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7171a618c666677d111fcc1e9c15cf3cb83f4f6ae4ff6943e145d122c2c1a7f4
-size 2628
+oid sha256:909678ef12459e257702901c024d779f63fe05c6998f972460cca1024f36a7dc
+size 2780
