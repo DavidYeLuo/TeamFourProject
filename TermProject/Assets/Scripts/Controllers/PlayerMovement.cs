e359bcfb2828df7fcc8be0c11c9e481dfd2289d7
DavidYeLuo <dayeluo@galileoweb.org>
2023-03-26 17:53:44 -0700

    Fix movement. Prefab wasn't saved.

--- TermProject/Assets/Scripts/Controllers/PlayerMovement.cs
+++ TermProject/Assets/Scripts/Controllers/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:591b561ca7cca00e5b3986723609b591f3276db7bb9fa91a09f5873beb84c360
-size 1742
+oid sha256:9b53bd6a93bba5792f34a070c8f08f0b2449e4e051385fcb48b813823ff15191
+size 1621
--- TermProject/Assets/Scripts/PlayerHandler.cs
+++ TermProject/Assets/Scripts/PlayerHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:909678ef12459e257702901c024d779f63fe05c6998f972460cca1024f36a7dc
-size 2780
+oid sha256:510174d81e7e2a28e92baed6a7a72d3ff28ad2ad26a1e760a0eac41e7d0ac95f
+size 2832
