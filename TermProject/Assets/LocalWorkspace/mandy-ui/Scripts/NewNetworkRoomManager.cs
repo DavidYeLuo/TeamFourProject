b12f5458a04c5bb012f8d48cbd20c77d02f64703
Mandy Noto <mandynoto@icloud.com>
2023-04-09 19:56:21 -0700

    feat(mandy-ui/Scripts): add scripts for offline, online, and game scene

---
+++ TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/NewNetworkRoomManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a516a6537cabd8d6d39cde685a5f238b4f160d05553c5161ccfe53bc25fa6439
+size 7426
---
+++ TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/NewNetworkRoomPlayer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f6c8cc31591743b19eb8b6b8852f8f7ff1c72ab6b30c2d46c2e47bfbb716ebe9
+size 5135
