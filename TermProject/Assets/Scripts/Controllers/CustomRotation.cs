f2e011c3be9104f1a2b2aecf987d1cf774b4b159
DavidYeLuo <davidayeluo@gmail.com>
2023-04-15 01:24:27 -0700

    Potential Addition to Allow Camera to Move Up and Down. [Bug] Currently, moving will reset the camera position to face the player. Therefore, it is just a comment

--- TermProject/Assets/Scripts/Controllers/CustomRotation.cs
+++ TermProject/Assets/Scripts/Controllers/CustomRotation.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a85cf378dbe7b331bddf6c68d5dbc932bdca0a9de403a8450896a18def3fd482
-size 609
+oid sha256:c10aa57232b7ab1b89e7457339b0a676f20d4998e23dca9aff87a61f7337428a
+size 865
