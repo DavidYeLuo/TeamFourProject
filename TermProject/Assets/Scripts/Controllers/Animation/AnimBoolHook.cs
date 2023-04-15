cb912bef148f35941815c182a94787aadc917b2f
DavidYeLuo <davidayeluo@gmail.com>
2023-04-15 00:13:06 -0700

    Save Prefab. Also Rework Animation Hooks.
    Animation hooks are done through many components now.

---
+++ TermProject/Assets/Scripts/Controllers/Animation/AnimBoolHook.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3fd236bd3b0bf76b861c89e1b218fc42aa176ec47cd1ac303893722e31bb0287
+size 728
---
+++ TermProject/Assets/Scripts/Controllers/Animation/AnimXZVectorHook.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:541f98c40b6dbe44753dfd2427964954dc0e5c9b5d1f6e0373444b8942844cfb
+size 1023
