2c4a4a5958d89427f787571b3c6a43e820dee4a6
DavidYeLuo <davidayeluo@gmail.com>
2023-04-25 00:01:12 -0700

    Rework ability. Now icons are stored in abilities as well as colors.

--- TermProject/Assets/Scripts/Controllers/Abilities/Ability.cs
+++ TermProject/Assets/Scripts/Controllers/Abilities/Ability.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bffa51a84a67cc022e349959db46b956b5e6860baf762b9b2edf60452ab607e6
-size 948
+oid sha256:612cccd917f0a307cc10e8baa2da95dab5ff142f5a2d2d2b6ea447aad5fe76b0
+size 1156
---
+++ TermProject/Assets/Scripts/Controllers/UI/ColorHook.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:48e846e9d27de876033d72b73c3159f1fe2bbeb93305950425a85eea45284754
+size 569
