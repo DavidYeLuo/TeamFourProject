626f355de421ec8b5979863481c2b5e05c1a7bb4
DavidYeLuo <davidayeluo@gmail.com>
2023-04-25 00:27:21 -0700

    Reformat ability code. Added basic ability UI transition.

--- TermProject/Assets/Scripts/Controllers/UI/ColorHook.cs
+++ TermProject/Assets/Scripts/Controllers/UI/ColorHook.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48e846e9d27de876033d72b73c3159f1fe2bbeb93305950425a85eea45284754
-size 569
+oid sha256:eaabe25be2995175644af7930f50f8b1adab90772f757a01e07b95535493aeac
+size 607
--- TermProject/Assets/Scripts/Controllers/UI/SpriteHook.cs
+++ TermProject/Assets/Scripts/Controllers/UI/SpriteHook.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94c11a54e0ebae0a43d8315b10918be940dc764149e17dbae88b2d01c3495cb1
-size 624
+oid sha256:849f335732d1f2f3dba276166b88371ca105af23b57c008bffeb4b860fb20c4e
+size 662
