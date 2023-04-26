5f32a6035f50d83e0a6e3ba9be7eb012a2c8f056
DavidYeLuo <davidayeluo@gmail.com>
2023-04-25 17:56:52 -0700

    When player is created, it will create an UI for each teammate.

--- TermProject/Assets/Scripts/Teammates/TeammatesHook.cs
+++ TermProject/Assets/Scripts/Teammates/TeammatesHook.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a9dd2dedcf09b1ecd7c63354ab7a0412d3d704167c762931fa6b9f49f02ec94
-size 520
+oid sha256:489aabb6e1d5bd41123ba1b874b1f5a62fbad36ee4b3330007d187937ac44e2e
+size 905
