cb98dbf6d667bab03956fd0c11b79ea0089c3b31
DavidYeLuo <davidayeluo@gmail.com>
2023-05-06 14:49:15 -0700

    Add an input system and playercontroller for network.

---
+++ TermProject/Assets/Scripts/Controllers/Multiplayer/MultiplayerInput.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c7076ac4e6f5c0115cc236ca9caa8e332643e8cc2b5f8090df8e29f5ea5c889
+size 4406
---
+++ TermProject/Assets/Scripts/Controllers/Multiplayer/MultiplayerPlayerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec4169dfa8c285eff16a2189375b53e7e7e63ccdda15385d3168a404dbcce670
+size 2215
