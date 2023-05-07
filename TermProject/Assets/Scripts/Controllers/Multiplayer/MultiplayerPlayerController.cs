26748246972855ccd2a9db927b3c0f0220b46d40
DavidYeLuo <davidayeluo@gmail.com>
2023-05-06 17:57:35 -0700

    Refactor player interaction. It is now based on the player's direction.

--- TermProject/Assets/Scripts/Controllers/Multiplayer/MultiplayerPlayerController.cs
+++ TermProject/Assets/Scripts/Controllers/Multiplayer/MultiplayerPlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52125b11bfd0bd5b18c5649bc69dcecaa649e324a811e82d5c74bc1c605bf257
-size 3336
+oid sha256:1a192c384c0da6938e63d8d9bd412f64ce9a993be271ff4eb097e584cf0f96de
+size 3368
