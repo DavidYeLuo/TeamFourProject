178eea81fab940c1b0dd443f6897e0033a13dae6
Christopher Ling <cling1@mail.sfsu.edu>
2023-05-12 11:19:38 -0700

    Changed the way things work so that HostButton is only on MultiPlayerMenuScene, and it passes the name to RoomManager's RoomManagerPlayerNameHolder, which caused me to change how the NewNetworkRoomPlayer was getting the username, also changed it so that when a player backs out to back menu it kicks everyone out... so that you can go in and out of room right now

--- TermProject/Assets/LocalWorkspace/Christopher-Spawning/NetworkScripts/HostButton.cs
+++ TermProject/Assets/LocalWorkspace/Christopher-Spawning/NetworkScripts/HostButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9699ca96b45daf548955bb9fdb1adb51bdf2ce3ece0d7bdbde93d8150fe6ca64
-size 863
+oid sha256:18d995b86c16f103fa60d19574568d1dacb1b50e6082f6e0a871c98764e0aa81
+size 2017
--- TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Multiplayer_Menu_Controller.cs
+++ TermProject/Assets/LocalWorkspace/mandy-ui/Scripts/Multiplayer_Menu_Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c80616e058127e82300452e65b2ef2dac04bfbfdaed284cd37817bc18bfc0be
-size 449
+oid sha256:812455be5d586cabc317bf29941485fdeada5188b006fbc1053876013efca308
+size 674
