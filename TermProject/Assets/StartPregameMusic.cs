695e7c8621173e5a6f8a8c13c45fb3247d75164a
Mandy Noto <mandynoto@icloud.com>
2023-05-13 20:41:35 -0700

    feat(ui-start): attach sound to updated script

--- TermProject/Assets/StartPregameMusic.cs
+++ TermProject/Assets/StartPregameMusic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38468cb1b08e1459b4c74c8db8b54effbf3d366725e083aaf3b166257b0b93d3
-size 320
+oid sha256:cc15cf3e31079c0c1d3f244a785952e1e41ac174e3727b57ff4d7a58db17e45f
+size 744
