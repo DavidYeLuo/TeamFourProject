e98cc4b58b02bfdec19df9f7a01c27283d1d3539
Marc Joseph Guerrero Castro <85776942+majocast@users.noreply.github.com>
2023-05-16 14:27:32 -0700

    Marc environment (#64)
    * inital envo layout, working on textures
    
    * inital envo layout, working on textures
    
    * inital envo layout, working on textures + other room
    
    * initial envo-1 & envo-2 layouts, will do textures tmrw
    
    * texturing and fixing of evno-1 and envo-2
    
    * added terrain and upgraded main room
    
    * terrain clean and navmesh fix
    
    * terrain clean and navmesh fix

---
+++ TermProject/Assets/LocalWorkspace/marc-environment/Components/Castlear Creation/Natura/Readme.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f55b10c7a909bf7a4ee75257be994a89ebc68905688169855d79265da4387a6
+size 1117
---
+++ TermProject/Assets/LocalWorkspace/marc-environment/Components/Old Columns/scripts/FlyCamera.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0bc5adddc2d771a19f4ddf38566377a4108f0d3fc6217d25888cfdbae73d28f1
+size 4003
